a49d253ce2a949b87dba5135b8f860782d8cd6c9
kvijay1710 <Vijay@Pixalive.me>
2024-11-11 21:50:57 +0530

    workflow del

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -2,11 +2,7 @@ import { CheckCircle2 } from "lucide-react"
 import codeImg from "../assets/code.jpg"
 const Workflow = () => {
     return (
-        <div className="mt-9">
-            <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide" >Accelerate your
-                <span className="bg-gradient-to-r from-orange-500 to-oran">coding workflow</span>
-            </h2>
-        </div>
+        <div></div>
     )
 }
 
