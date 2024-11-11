316ba87f174dd8711250f2778b83e128efa91d0c
kvijay1710 <Vijay@Pixalive.me>
2024-11-11 21:38:41 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import FeatureSection from "./components/FeatureSection"
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
+import Workflow from "./components/Workflow"
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-5 px-6">
+        <HeroSection />
+        <FeatureSection/>
+        <Workflow/>
+      </div>
+    </>
+  )
+}
+
+export default App
