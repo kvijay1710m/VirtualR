746dfc2240c71defafd165790ec9541a20cf368c
kvijay1710 <Vijay@Pixalive.me>
2024-11-12 10:01:58 +0530

    pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import Pricing from "./components/Pricing"
 import Workflow from "./components/Workflow"
 
 
@@ -9,10 +10,11 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="max-w-7xl mx-auto pt-5 px-6">
+      <div className="max-w-6xl mx-auto pt-5 px-6">
         <HeroSection />
         <FeatureSection/>
         <Workflow/>
+        <Pricing/>
       </div>
     </>
   )
--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,6 +1,7 @@
 import { CheckCircle2 } from "lucide-react"
 import codeImg from "../assets/code.jpg"
 import { checklistItems } from "../constants"
+
 const Workflow = () => {
     return (
         <div className="mt-9">
