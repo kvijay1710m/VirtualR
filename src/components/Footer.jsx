20ce62bf50c2fe43a99072b4dcc860eac0ddf206
kvijay1710 <Vijay@Pixalive.me>
2024-11-12 11:26:33 +0530

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { resourcesLinks, platformLinks, communityLinks } from "../constants"
+const Footer = () => {
+    return (
+        <>
+            <footer className="mt-20 mb-5 border-t border-neutral-700">
+                <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div>
+                        <h3 className="tex-mdfont-semibold mb-4" >Resources</h3>
+                        <ul className="space-y-2">
+                            {resourcesLinks.map((link, index) => (
+                                <li key={index}>
+                                    <a className="text-neutral-300 hover:text-white" href="">{link.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="tex-mdfont-semibold mb-4" >Platform</h3>
+                        <ul className="space-y-2">
+                            {platformLinks.map((platform, index) => (
+                                <li key={index}>
+                                    <a className="text-neutral-300 hover:text-white" href="">{platform.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="tex-mdfont-semibold mb-4" >Community</h3>
+                        <ul className="space-y-2">
+                            {communityLinks.map((community, index) => (
+                                <li key={index}>
+                                    <a className="text-neutral-300 hover:text-white" href="">{community.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
